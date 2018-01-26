2356464a6663b92ea32454d6b5977fc2e54fd603
Xiebaobiao <1298568873>
2018-01-26 11:39:11 +0800

    反射

--- J2SE/src/lang/Class/Person.java
+++ J2SE/src/lang/Class/Person.java
@@ -7,14 +7,14 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class Person implements Serializable {
+public class Person {
     private String name;
     private int age;
 
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
+//    public Person(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
 
     public int getAge() {
         return age;
@@ -32,17 +32,44 @@ public class Person implements Serializable {
         return name;
     }
 
-    public static void main(String[] args) throws IntrospectionException, InvocationTargetException, IllegalAccessException {
-        Person person = new Person("ming", 22);
-        Class clazz = person.getClass();
-        Field[] fields = clazz.getDeclaredFields();
-        for (Field f : fields) {
-            String key = f.getName();
-            PropertyDescriptor propertyDescriptor = new PropertyDescriptor(key, clazz);
-            Method method=propertyDescriptor.getReadMethod();
-            Object value = method.invoke(person);
+    //    public static void main(String[] args) throws IntrospectionException, InvocationTargetException, IllegalAccessException {
+//        Person person = new Person("ming", 22);
+//        Class clazz = person.getClass();
+//        Field[] fields = clazz.getDeclaredFields();
+//        for (Field f : fields) {
+//            String key = f.getName();
+//            PropertyDescriptor propertyDescriptor = new PropertyDescriptor(key, clazz);
+//            Method method=propertyDescriptor.getReadMethod();
+//            Object value = method.invoke(person);
+//
+//            System.out.println(key+":"+value);
+//        }
+//    }
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, NoSuchFieldException, InvocationTargetException {
+        Class<?> aClass = Class.forName("lang.Class.Person");
+        Person p = (Person) aClass.newInstance();
 
-            System.out.println(key+":"+value);
+        /**
+         * 获取属性
+         */
+        Field field1 = aClass.getDeclaredField("name");//获取指定的属性
+        System.out.println("属性：" + field1);
+        Field[] declaredFields = aClass.getDeclaredFields();//获取所有属性
+        for (Field f : declaredFields) {
+            System.out.println("属性：" + f);
         }
+
+        /**
+         * 获取方法
+         */
+        Method setName = aClass.getDeclaredMethod("setName", new Class[]{String.class});//获取指定的方法
+        System.out.println("method1:" + setName);
+        Method[] declaredMethods = aClass.getDeclaredMethods();//获取所有方法
+        for (Method m : declaredMethods) {
+            System.out.println("method：" + m);
+        }
+
+        Object invoke = setName.invoke(p, new String[]{"a"});
+        System.out.println(p.getName());
     }
 }
